760de6be05faa3c6e6d6394affa3010876d11645
IKEH <pondau1024@gmail.com>
2020-01-23 20:51:21 +0900

    設定用のStoryboardを作成

---
+++ FontPanelSample/PreferencesTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesTabViewController.swift
+//  FontPanelSample
+//
+//  Created by Hiroki Ikeuchi on 2020/01/23.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesTabViewController: NSTabViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ FontPanelSample/PreferencesWindow.swift
@@ -0,0 +1,13 @@
+//
+//  PreferencesWindow.swift
+//  FontPanelSample
+//
+//  Created by Hiroki Ikeuchi on 2020/01/23.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindow: NSPanel {
+
+}
